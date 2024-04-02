8ff289383508a0ef806f3296fdeb753c386350ff
TimserGroup <135937461+TimserGroup@users.noreply.github.com>
2024-04-01 22:59:20 -0600

    correcion node

--- src/config.js
+++
@@ -1,6 +0,0 @@
-export const PORT = process.env.PORT || 3000;
-export const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb+srv://timsercrm:preventixpass20@cluster0.xfum3w2.mongodb.net/";
-export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
-
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
